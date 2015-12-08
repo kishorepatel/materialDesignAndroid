a97de61021caa4d9ed70dcc96b1477a0ada86ae0
kishorepatel <kishore.patel.india@gmail.com>
2015-12-08 16:12:16 +0530

    Add L4, main->detailActivitiy transition, IMP: see v21 styles.xml

---
+++ L4_06_tut/app/src/main/java/in/blogspot/upsolving/l4_06_tut/ActivityDetail.java
@@ -0,0 +1,16 @@
+package in.blogspot.upsolving.l4_06_tut;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by NEW on 08-12-2015.
+ */
+public class ActivityDetail extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ L4_06_tut/app/src/main/java/in/blogspot/upsolving/l4_06_tut/MainActivity.java
@@ -0,0 +1,65 @@
+package in.blogspot.upsolving.l4_06_tut;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    GridView gridView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridView = (GridView) findViewById(R.id.grid);
+        gridView.setAdapter( new GridAdapter());
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                startActivity(new Intent(MainActivity.this, ActivityDetail.class),
+                        ActivityOptions.makeSceneTransitionAnimation(MainActivity.this).toBundle());
+            }
+        });
+    }
+
+    public class GridAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 20;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if(convertView == null){
+                convertView = getLayoutInflater().inflate(R.layout.grid_item, parent, false);
+            }
+
+            ((TextView)convertView).setText("A");
+            return convertView;
+        }
+    }
+}
